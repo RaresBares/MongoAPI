11ae5f2c00c34571491f29e06523813a7c3a70b0
Rares <rsahleanu@gmail.com>
2020-02-10 07:52:35 +0100

    Init

--- src/test/java/de/mymongo/client/collection/TargetCollection.java
+++ src/test/java/de/mymongo/client/collection/TargetCollection.java
@@ -1,9 +1,11 @@
 package de.mymongo.client.collection;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class TargetCollection extends DBCollection {
@@ -12,5 +14,31 @@ public class TargetCollection extends DBCollection {
     }
 
 
-    public List<DBObject>  
+    public List<DBObject> objects(){
+        return getHintFields();
+    }
+    public List<DBObject> get(HashMap<String, String> values){
+        return find(new BasicDBObject(values)).toArray();
+    }
+
+    public void delete(HashMap<String, String> values){
+        for (DBObject dbObject : get(values)) {
+            remove(dbObject);
+        }
+
+    }
+
+    public DBObject fetch(HashMap<String, String> values){
+        DBObject object = new BasicDBObject(values);
+        return object;
+    }
+
+    public void create(HashMap<String, String> values){
+        insert(fetch(values));
+    }
+
+    public void change(HashMap<String, String> values){
+        BasicDBObject query = new BasicDBObject();
+        query.put("name", "Shubham");
+    }
 }
